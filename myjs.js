6b9d3bb954caa6e49006bd3c86002caa63066812
Madhav Jadhav <86906584+jadhavmadhav@users.noreply.github.com>
2021-11-27 01:59:11 +0530

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -1,17 +1,34 @@
-function toggle(){
-    document.getElementById("leftside").style.display="block"
-    document.getElementById("toggle-btn").style.display="none"
-    document.getElementById("close").style.display="block"; 
-   
+function toggle() {
+
+    const sidebar = document.getElementById("leftside")
+    if (sidebar.style.display == "block") {
+        sidebar.style.display = "none"
+    } else {
+        sidebar.style.display = "block"
+        document.getElementById("toggle-btn").style.left = "10px"
+    }
+
 
 }
 
- function Close(){
- document.getElementById("sidebar").style.display="none"
- document.getElementById("close").style.display="none";
- document.getElementById("toggle-btn").style.display="block"
- window.onclick(myfun());
-  function myfun(){
-    document.getElementById("close").style.display="none";
-   }
- }
+function sidebarnone() {
+    const sidebar = document.getElementById("leftside")
+    sidebar.style.display = "none"
+}
+
+
+
+function contactdata() {
+    event.preventDefault();
+    const form = document.getElementById("myform")
+    const name = form.elements["name"].value;
+    const email = form.elements["email"].value;
+    const subject = form.elements["subject"].value;
+    const message = form.elements["message"].value;
+
+    alert(`
+     Name: ${name} 
+     Email: ${email} 
+     Subject: ${subject} 
+     Message: ${message}`)
+}
